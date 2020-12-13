f549c019f446f23713cd5a8604725b6e02fe269e
Zaneta <zaneta@Zanetas-Mac.local>
2020-12-13 04:27:30 -0800

    Initial Commit

---
+++ PopraviDefekt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopraviDefekt
+//
+//  Created by Zaneta on 12/13/20.
+//  Copyright © 2020 Zaneta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
